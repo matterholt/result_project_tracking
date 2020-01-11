79ed2a46862ec4c9d467bcd514b5436406387fe1
MAtterholt <matterholt84@gmail.com>
2020-01-11 04:47:45 -0500

    file watcher created.

---
+++ result_processing/execute.py
@@ -0,0 +1,9 @@
+"""
+main entry for building/submitting results to the 
+
+"""
+def execute_process(watch_template_file):
+    """
+    Params: the template file that is being watched and updated
+    """
+    print("add the data")
