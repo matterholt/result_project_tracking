9657f8a2f26dcd851758ee1b3fb7bb58d28cdf67
MAtterholt <matterholt84@gmail.com>
2020-05-14 18:27:49 -0400

    model builds,

--- resultTracker/resultTracker/settings.py
+++ resultTracker/resultTracker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n+z35(p0r38_(yk6g*9whrz%c-@$sb1dtz+d+cu6u-2oq%85+x'
+SECRET_KEY = 'test'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'results.apps.ResultsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- resultTracker/results/models.py
+++ resultTracker/results/models.py
@@ -1,40 +1,25 @@
-# Admin.py
-from django.contrib import admin
-#django_enf
-# Register your models here.
-from .models import Detail_cm_model,Stiffness_result, Model_files,Project
-
-admin.site.register(Detail_cm_model)
-admin.site.register(Stiffness_result)
-admin.site.register(Model_files)
-admin.site.register(Project)
-
-
-# models.py
-# Need to confirm is correct, could test on office machine
-import datetime
-
 from django.db import models
 from django.utils import timezone
-
-# Create your models here.
 
 
 class Project(models.Model):
     project_code = models.CharField(max_length=150)
     project_part = models.CharField(max_length=150)
+
     def __str__(self):
         return self.project_code
-    
+
+
 class Model_files(models.Model):
-    cm_result_pnch_file : models.FileField(max_length=1000)
-    
+    cm_result_pnch_file: models.FileField(max_length=1000)
+
+
 class Stiffness_result(models.Model):
     location_load_applied = models.CharField(max_length=100)
     displacement_xyz = models.CharField(max_length=100)
-    node_number = models.IntegerField()
+    node_number = models.IntegerField(default=0)
     load_direction = models.CharField(max_length=100)
-    newton_mm_force = models.IntegerField()
+    newton_mm_force = models.IntegerField(default=0)
 
 
 class Detail_cm_model(models.Model):
@@ -43,8 +28,9 @@ class Detail_cm_model(models.Model):
     date_added = models.DateField(auto_now_add=True)
     cm_model_description = models.CharField(max_length=100)
     project_code = models.ForeignKey(Project, on_delete=models.CASCADE)
-    model_files = models.ForeignKey()
-    stiffness_results = models.ForeignKey()
+    model_files = models.ForeignKey(Model_files, on_delete=models.CASCADE)
+    stiffness_results = models.ForeignKey(
+        Stiffness_result, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.cm_model_name
