a94b780b6c1c62971e7748f43ebf9e97dc805ff1
MAtterholt <matterholt84@gmail.com>
2020-05-16 19:06:30 -0400

    post method for route

--- resultTracker/results/views.py
+++ resultTracker/results/views.py
@@ -8,6 +8,16 @@ from .serializers import Detail_cm_model_Serializer
 
 @api_view(['GET', 'POST'])
 def result_list(request):
-    data = Detail_cm_model.objects.all()
-    serializer = Detail_cm_model_Serializer(data, context={'request': request})
-    return Response(serializer.data)
+    if request.method == 'GET':
+        data = Detail_cm_model.objects.all()
+        serializer = Detail_cm_model_Serializer(
+            data, context={'request': request})
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = Detail_cm_model_Serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
