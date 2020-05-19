84dfd87db32e9f1b6ec9e02274d856325487a9fc
MAtterholt <matterholt84@gmail.com>
2020-05-18 22:36:59 -0400

    cleaned up folders

--- resultTracker/python_Script_code/code/pkg/error_comments.py
+++
@@ -1,9 +0,0 @@
-import sys
-def sql_header_error(issue):
-    desc = """
-    Error has accrued !!!
-    Template file does not have correct column names
-    Database contains => \n
-    """ 
-    error = desc + issue + "\n"
-    sys.exit()
